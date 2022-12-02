7ec48c6d11ec6b8f405ac348e4ee7335ba8f2e67
Stray8 <2086312274@qq.com>
2022-12-02 23:11:34 +0800

    delete build and devel

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liudandi/dff_ws/src
-# Build directory: /home/liudandi/dff_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("Quarotor_Feedback_Controller")
--- build/Quarotor_Feedback_Controller/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/liudandi/dff_ws/src/Quarotor_Feedback_Controller/include".split(';') if "/home/liudandi/dff_ws/src/Quarotor_Feedback_Controller/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "Quarotor_Feedback_Controller"
-PROJECT_SPACE_DIR = "/home/liudandi/dff_ws/devel"
-PROJECT_VERSION = "0.0.0"
